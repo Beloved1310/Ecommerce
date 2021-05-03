5c8e43d44cd72c94e4ade15506bc07b275174010
Beloved1310 <adeoluwafisayomi@gmail.com>
2021-05-03 11:34:38 +0100

    desc: create the entry file for all routes

---
+++ index.js
@@ -0,0 +1,31 @@
+// const error = require('./middleware/error');
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+const mongoose = require("mongoose");
+const user = require("./Routes/user");
+const product = require("./Routes/product");
+const purchase = require('./Routes/order')
+const debug = require("debug")("app");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+}).then(() => console.log('Connected to MongoDB...'))
+.catch(err => console.error('Could not connect to MongoDB...', err))
+
+app.use("/", user);
+app.use("/", product);
+app.use("/", purchase);
+// app.use(error);
+
+const PORT = process.env.PORT || 7000;
+
+app.listen(PORT, () => {
+  debug(`Web server is running ${PORT}`);
+});
