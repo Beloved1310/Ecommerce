e883422a8d8bea07ad9b5896b95febc50806d239
Beloved1310 <adeoluwafisayomi@gmail.com>
2021-05-06 12:49:50 +0100

    docs: create routes for checkout orders, user view of purchased products, payment with flutterwave

--- Routes/order.js
+++ Routes/order.js
@@ -1,126 +1,121 @@
-const asyncMiddleware = require("../middleware/async");
-const express = require("express");
-const User = require("../Model/User");
-const Payment = require("../Model/Payment");
-const Product = require("../Model/Products");
-const Order = require("../Model/Order");
-const auth = require("../middleware/auth");
+const asyncMiddleware = require('../middleware/async');
+const express = require('express');
+const Payment = require('../Model/Payment');
+const Product = require('../Model/Products');
+const Order = require('../Model/Order');
+const auth = require('../middleware/auth');
 const router = express.Router();
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const axios = require("axios");
+const axios = require('axios');
 
 // ....................pass order id
 router.get(
-  "/order/:id",
+  '/order/:id',
   auth,
   asyncMiddleware(async (req, res) => {
-    const order = await Order.findById(req.params.id).populate("product");
+    const order = await Order.findById(req.params.id).populate('product');
     if (order) {
       res.send(order);
     } else {
-      res.status(404).send({ message: "Order Not Found" });
+      res.status(404).send({ message: 'Order Not Found' });
     }
   })
 );
 
-// >>>>>>>>>>>>>pass user token
-// >>>>>>>>>>>>>>> see my purchase product
+//...................pass user token
+//...................see my purchase product
 router.get(
-  "/order/user/mine",
+  '/order/user/mine',
   auth,
   asyncMiddleware(async (req, res) => {
     const orders = await Order.find({ user: req.user._id }).populate(
-      "user",
-      "fullname email -_id"
+      'user',
+      'fullname email -_id'
     );
-    console.log(orders);
     res.send(orders);
   })
 );
 
 router.post(
-  "/order",
+  '/order',
   auth,
   asyncMiddleware(async (req, res) => {
-
     const headers = {
       authorization: process.env.SECRET_KEY,
-      "content-type": "application/json",
-      "cache-control": "no-cache",
+      'content-type': 'application/json',
+      'cache-control': 'no-cache',
     };
     const response = await axios.post(
-        "https://api.flutterwave.com/v3/payments",
-        req.body,
-        { headers }
-      );
-      const responseLink = response.data
+      'https://api.flutterwave.com/v3/payments',
+      req.body,
+      { headers }
+    );
+    const responseLink = response.data;
+    const {
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      shippingPrice,
+      taxPrice,
+      totalPrice,
+    } = req.body;
+
     const existProduct = Product.findById(req.body.productId);
     if (!existProduct) {
-      res.status(404).send({ message: "Product not found" });
+      res.status(404).send({ message: 'Product not found' });
     } else {
       const order = new Order({
         _id: mongoose.Types.ObjectId(),
         product: req.body.productId,
-        shippingAddress: req.body.shippingAddress,
-        paymentMethod: req.body.paymentMethod,
-        itemsPrice: req.body.itemsPrice,
-        shippingPrice: req.body.shippingPrice,
-        taxPrice: req.body.taxPrice,
-        totalPrice: req.body.totalPrice,
+        shippingAddress,
+        paymentMethod,
+        itemsPrice,
+        shippingPrice,
+        taxPrice,
+        totalPrice,
         user: req.user._id,
       });
       const createdOrder = await order.save();
-      
-      res
-        .status(201)
-        .send({ message: "New Order Created", order: createdOrder._id, responseLink });
+
+      res.status(201).send({
+        message: 'New Order Created',
+        order: createdOrder._id,
+        responseLink,
+      });
     }
   })
 );
 
-router.post("/initialise/payment", auth, asyncMiddleware(async(req,res) => {
-  const headers = {
-          authorization: process.env.SECRET_KEY,
-          "content-type": "application/json",
-          "cache-control": "no-cache",
-        };
+router.post(
+  '/initialise/payment',
+  auth,
+  asyncMiddleware(async (req, res) => {
+    const headers = {
+      authorization: process.env.SECRET_KEY,
+      'content-type': 'application/json',
+      'cache-control': 'no-cache',
+    };
 
-        const response = await axios.post(
-                  "https://api.flutterwave.com/v3/payments",
-                  req.body,
-                  { headers }
-            )
-            if (response){
-              res.send(response.data)
-            }else{
-              res.send(error)
-            }
-}))
-
-// router.post(
-//   "/initialise/payment",
-//   auth,
-//   asyncMiddleware(async (req, res) => {
-
-//     const headers = {
-//       authorization: process.env.SECRET_KEY,
-//       "content-type": "application/json",
-//       "cache-control": "no-cache",
-//     };
-//     const response = await axios.post(
-//         "https://api.flutterwave.com/v3/payments",
-//         req.body,
-//         { headers }
-//   )});
-
+    const response = await axios.post(
+      'https://api.flutterwave.com/v3/payments',
+      req.body,
+      { headers }
+    );
+    if (response) {
+      res.send(response.data);
+    } else {
+      res.send(error);
+    }
+  })
+);
 
 router.post(
-  "/webhook/details",
+  '/webhook/details',
   asyncMiddleware(async (req, res) => {
-    var hash = req.headers["verify-hash"];
+    var hash = req.headers['verify-hash'];
     if (hash !== process.env.MY_HASH) {
-      res.send(401, "Unauthorized User");
+      res.send(401, 'Unauthorized User');
     } else {
       const { data, status, message } = req.body;
       const createdWebhook = await Payment.create({
@@ -134,29 +129,26 @@ router.post(
 );
 
 router.get(
-  "/verify/flutterwave/verify/:ref",
+  '/verify/flutterwave/verify/:ref',
   asyncMiddleware(async (req, res) => {
-    
-
     const ref = req.params.ref;
     const response = await axios.get(
       `https://api.flutterwave.com/v3/transactions/${ref}/verify/`,
       {
         headers: {
           authorization: process.env.SECRET_KEY,
-          "content-type": "application/json",
-          "cache-control": "no-cache",
+          'content-type': 'application/json',
+          'cache-control': 'no-cache',
         },
       }
     );
 
-     if (response.data.status == 'success') {
-       await Order.updateOne({}, {isPaid:true});
-      console.log({response : response.data, order: "Paid"})
+    if (response.data.status == 'success') {
+      await Order.updateOne({}, { isPaid: true });
+      res.send({ response: response.data, order: 'Paid' });
     } else {
       res.send(error);
     }
-    
   })
 );
 
