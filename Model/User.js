489501556667c25c16b407fc97d01b59404e5bdf
Beloved1310 <adeoluwafisayomi@gmail.com>
2021-05-06 12:43:58 +0100

    docs: create userschema for user infomation to be stored in the database

--- Model/User.js
+++ Model/User.js
@@ -1,24 +1,23 @@
-const jwt = require("jsonwebtoken");
-const mongoose = require("mongoose");
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
   fullname: {
     type: String,
-    required : true,
+    required: true,
   },
   email: {
     type: String,
-    required : true,
+    required: true,
   },
   password: {
     type: String,
-    required : true,
+    required: true,
   },
   gender: String,
   age: Number,
-  education : {
+  education: {
     grade: String,
-    
   },
   experience: {
     position: String,
@@ -27,14 +26,16 @@ const UserSchema = new mongoose.Schema({
   },
   resetLink: {
     date: String,
-    default: "",
+    default: '',
   },
 });
 
 UserSchema.methods.generateAuthToken = function () {
-  const token = jwt.sign({ _id: this._id ,fullname: this._fullname,
-    email: this.email}, process.env.JWT_KEY);
+  const token = jwt.sign(
+    { _id: this._id, fullname: this._fullname, email: this.email },
+    process.env.JWT_KEY
+  );
   return token;
 };
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model('User', UserSchema);
