571ec489a4bd98929ee6cd26ed4e38d89f84a07e
Beloved1310 <adeoluwafisayomi@gmail.com>
2021-05-06 12:23:02 +0100

    docs: create paymentschema for infomation to be stored in the database

--- Model/Payment.js
+++ Model/Payment.js
@@ -1,18 +1,19 @@
 const mongoose = require('mongoose');
- const PaymentSchema = new mongoose.Schema({
- status: String,
- message: String,
+const PaymentSchema = new mongoose.Schema({
+  status: String,
+  message: String,
 
- data : {
-   id: Number,
-   amount: Number,
-   currency : String,
-   created_at : String,
-   customer: {
-     id: Number,
-     name: String,
-     email: String
-   },
- }});
+  data: {
+    id: Number,
+    amount: Number,
+    currency: String,
+    created_at: String,
+    customer: {
+      id: Number,
+      name: String,
+      email: String,
+    },
+  },
+});
 
 module.exports = mongoose.model('Payment', PaymentSchema);
