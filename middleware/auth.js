39f8ba0b5c2d646d5e8a16970dd697be3076dcb4
Beloved1310 <adeoluwafisayomi@gmail.com>
2021-05-14 15:16:18 +0100

    fix: read all environment variable from config #5

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,14 +1,19 @@
+/* eslint consistent-return: "off" */
+
 const jwt = require('jsonwebtoken');
+const { JWT } = require('../config');
 
 module.exports = (req, res, next) => {
   const token = req.header('x-auth-token');
-  if (!token) { return res.status(401).send('Access denied. No token provided'); }
+  if (!token) {
+    return res.status(401).send('Access denied. No token provided');
+  }
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_KEY);
+    const decoded = jwt.verify(token, JWT);
     req.user = decoded;
     next();
   } catch (ex) {
     res.status(400).send('Invalid token.');
   }
-}; 
+};
