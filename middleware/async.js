35ee05bb39d14d969584ded60e90eb917738d096
Beloved1310 <adeoluwafisayomi@gmail.com>
2021-05-14 15:03:02 +0100

    feat: customize error message

--- middleware/async.js
+++ middleware/async.js
@@ -3,6 +3,7 @@ module.exports = function asyncMiddleware(handler) {
     try {
       await handler(req, res);
     } catch (ex) {
+      res.status(ex.status || 500).send({ message: ex.message });
       next(ex);
     }
   };
