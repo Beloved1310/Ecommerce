4bda18615ebee2e71d977d0b8abcce236d45c3f4
Beloved1310 <adeoluwafisayomi@gmail.com>
2021-05-15 09:31:37 +0100

    feat: validate activate payment request body #10

---
+++ validation/activatePassword.js
@@ -0,0 +1,8 @@
+const Joi = require('joi');
+
+module.exports = function validate(req) {
+  const schema = Joi.object({
+    token: Joi.string().max(900).required(),
+  });
+  return schema.validate(req);
+};
