39c5a43b18a73bca7183456425f5f74fa2d19ed8
Beloved1310 <adeoluwafisayomi@gmail.com>
2021-05-20 14:18:07 +0100

    feat: correct joi validation

--- validation/signupValidation.js
+++ validation/signupValidation.js
@@ -2,18 +2,18 @@ const Joi = require('joi');
 
 module.exports = function validate(req) {
   const schema = Joi.object({
-    fullname: Joi.string().min(5).required(),
+    fullname: Joi.string().min(5).label('First Name').required(),
     email: Joi.string().email().min(3).max(300).required(),
-    password: Joi.string().min(5).max(255).required(),
-    gender: Joi.string().min(1).required(),
+    password: Joi.string().Joi.number().min(5).max(255).required(),
+    gender: Joi.any().valid('F', 'M').required(),
     age: Joi.number().min(1).required(),
     education: Joi.object().keys({
-      grade: Joi.alternatives().try(Joi.string()),
+      grade: Joi.any().optional(),
     }),
     experience: Joi.object().keys({
-      position: Joi.alternatives().try(Joi.string()),
-      company: Joi.alternatives().try(Joi.string()),
-      location: Joi.alternatives().try(Joi.string()),
+      position: Joi.any().optional().string(),
+      company: Joi.any().optional().alphanum(),
+      location: Joi.any().optional().alphanum(),
     }),
   });
   return schema.validate(req);
