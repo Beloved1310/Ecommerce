b13a1bb43ec8b18b2476f84211150ef39b157f9b
Beloved1310 <adeoluwafisayomi@gmail.com>
2021-05-22 14:43:17 +0100

    feat: add joi validation

--- validation/signupValidation.js
+++ validation/signupValidation.js
@@ -6,15 +6,6 @@ module.exports = function validate(req) {
     email: Joi.string().email().min(3).max(300).required(),
     password: Joi.string().alphanum().min(5).max(255).required(),
     gender: Joi.any().valid('F', 'M').required(),
-    age: Joi.number().min(1).required(),
-    education: Joi.object().keys({
-      grade: Joi.any().optional(),
-    }),
-    experience: Joi.object().keys({
-      position: Joi.any().optional(),
-      company: Joi.any().optional(),
-      location: Joi.any().optional(),
-    }),
   });
   return schema.validate(req);
 };
