78c304c84b5806eddccff6e75ce2189edf8c7289
Beloved1310 <adeoluwafisayomi@gmail.com>
2021-05-22 14:45:58 +0100

    feat: re-write code

--- Routes/user.js
+++ Routes/user.js
@@ -21,6 +21,9 @@ const loginvalidate = require('../validation/loginValidate');
 const forgetpassword = require('../validation/forgetPassword');
 const newpassword = require('../validation/newPassword');
 const activatePassword = require('../validation/activatePassword');
+const emailData = require('../utilis/activation');
+const activationData = require('../utilis/emailactivation');
+const mailData = require('../utilis/forgotpassword');
 
 const transporter = nodemailer.createTransport(
   sendgridTransport({
@@ -36,43 +39,42 @@ router.post(
     const { error } = validate(req.body);
     if (error) return res.status(400).send({ error: error.details[0].message });
 
-    const {
+    const user = await User.findOne().select('email');
+    if (user) {
+      res.status(400).send({ message: 'User already registered' });
+    }
+
+    const { fullname, email, password, gender } = req.body;
+
+    const createdUser = new User({
       fullname,
       email,
       password,
-      age,
       gender,
-      education,
-      experience,
-    } = req.body;
-
-    const user = await User.findOne({ email });
-    if (user)
-      return res.status(400).send({ message: 'User already registered' });
-
-    const token = jwt.sign(
-      { fullname, email, password, age, gender, education, experience },
-      ACTIVATION_KEY
-    );
-
-    const emailData = {
-      to: email,
-      from: 'fisayo@foodcrowdy.com',
-      subject: 'Email Activation',
-      html: `<h2> Please Click on this link to verify your email </h2>
-      <p> Click on this <a href = "http://localhost:7000/authentication/activate/${token}">link </a>to reset password</p>
-      `,
-    };
-    transporter.sendMail(emailData, (ex) => {
-      if (ex) {
-        return res.send({ ex: ex.message });
-      }
-      const data = { user };
-      return res.send({
-        message: 'Email has been sent, kindly activate your email',
-        data,
-      });
     });
+    const salt = await bcrypt.genSalt(10);
+    createdUser.password = await bcrypt.hash(createdUser.password, salt);
+    const savedUser = await createdUser.save();
+
+    if (savedUser) {
+      const token = jwt.sign({ email }, ACTIVATION_KEY);
+
+      const sendEmail = await transporter.sendMail(
+        emailData(email, token, req)
+      );
+      if (!sendEmail) {
+        res.send({ error: error.message });
+      } else {
+        const data = {
+          fullname,
+          email,
+        };
+        return res.send({
+          message: 'Email has been sent, kindly activate your email',
+          data,
+        });
+      }
+    }
   })
 );
 
@@ -84,46 +86,12 @@ router.post(
     const { token } = req.body;
     if (token) {
       const decodedToken = jwt.verify(token, ACCTIVATION_KEY);
-      const {
-        fullname,
-        email,
-        password,
-        age,
-        gender,
-        education,
-        experience,
-      } = decodedToken;
-      const user = await User.findOne({ email });
-      if (user)
-        return res.status(400).send({ message: 'User already registered' });
-      const createdUser = new User({
-        fullname,
-        email,
-        password,
-        age,
-        gender,
-        education,
-        experience,
-      });
+      const { email } = decodedToken;
 
-      const salt = await bcrypt.genSalt(10);
-      createdUser.password = await bcrypt.hash(createdUser.password, salt);
-
-      const savedUser = await createdUser.save();
-      if (savedUser) {
-        const data = {
-          to: email,
-          from: 'fisayo@foodcrowdy.com',
-          subject: 'Confirmation Email',
-          html: `<p> Welcome to E-commerce Website, Do proceed to the <a href = "https://${req.headers.host}/login">Login Page</a></p>`,
-        };
-        transporter.sendMail(data);
-      }
-      const data = { fullname, email };
-      res.status(200).send({ message: 'Registered User', data });
-    } else {
-      return res.send({ error: 'Something went wrong' });
+      transporter.sendMail(activationData(email, req));
     }
+
+    res.status(200).send({ message: 'Email Activated', data: null });
   })
 );
 
@@ -158,41 +126,27 @@ router.post(
   asyncMiddleware(async (req, res) => {
     const { error } = forgetpassword(req.body);
     if (error) return res.status(400).send({ error: error.details[0].message });
+
     const { email } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .send({ error: 'User with this email does not exists' });
+    }
+    const token = jwt.sign({ _id: user.id }, FORGOT_PASSWORD, {
+      expiresIn: '20m',
+    });
+    const update = await user.updateOne({ resetLink: token });
+    if (!update) {
+      return res.status(400).send({ error: 'reset password link error' });
+    }
+    transporter.sendMail(mailData(email, token, req));
 
-    await User.findOne({ email }, (err, user) => {
-      if (!user) {
-        return res
-          .status(400)
-          .send({ error: 'User with this email does not exists' });
-      }
-
-      const token = jwt.sign({ _id: user.id }, FORGOT_PASSWORD, {
-        expiresIn: '20m',
-      });
-
-      const mailData = {
-        to: email,
-        from: 'fisayo@foodcrowdy.com',
-        subject: 'Password reset',
-        html: `
-        <h1> You requested for password reset</h1>
-        <p> Click on this <a href = "http://localhost:7000/forgotpassword/${token}">link </a>to reset password</p>
-        `,
-      };
-
-      return user.updateOne({ resetLink: token }, (ex) => {
-        if (ex) {
-          return res.status(400).send({ error: 'reset password link error' });
-        }
-        transporter.sendMail(mailData);
-
-        const data = { email };
-        return res.send({
-          message: 'Email has been sent, kindly follow the instructions',
-          data,
-        });
-      });
+    const data = { email };
+    return res.send({
+      message: 'Email has been sent, kindly follow the instructions',
+      data,
     });
   })
 );
@@ -204,7 +158,7 @@ router.post(
     if (error) return res.status(400).send({ error: error.details[0].message });
     const { Link, newPass } = req.body;
     const user = await User.findOne({ resetLink: Link });
-    if (!user) return res.status(422).send({ error: 'Try Again' });
+    if (!user) return res.send({ error: 'Try Again' });
 
     const hashedpassword = await bcrypt.hash(newPass, 12);
     if (hashedpassword) {
