4a21cef33d3783e4d4341fcd564574c899f3ddaa
Beloved1310 <adeoluwafisayomi@gmail.com>
2021-05-24 12:15:37 +0100

    feat: change joi validation name

--- validation/signupValidation.js
+++ validation/signupValidation.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 
 module.exports = function validate(req) {
   const schema = Joi.object({
-    fullname: Joi.string().min(5).label('First Name').required(),
+    fullname: Joi.string().min(5).label('fullname').required(),
     email: Joi.string().email().min(3).max(300).required(),
     password: Joi.string().alphanum().min(5).max(255).required(),
     gender: Joi.string().valid('F', 'M').required(),
