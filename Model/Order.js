6771937f355bd85cacf7ce784e02f878ee62982f
Beloved1310 <adeoluwafisayomi@gmail.com>
2021-06-01 09:55:34 +0100

    refactor: change order model

--- Model/Order.js
+++ Model/Order.js
@@ -6,9 +6,7 @@ const OrderSchema = new mongoose.Schema(
     products: [
       {
         name: String,
-
         price: Number,
-
         quantity: Number,
       },
     ],
@@ -32,42 +30,3 @@ const OrderSchema = new mongoose.Schema(
 );
 
 module.exports = mongoose.model('Order', OrderSchema);
-
-// const mongoose = require('mongoose');
-
-// const OrderSchema = new mongoose.Schema(
-//   {
-//     user: {},
-//     products: [
-//       {
-//     image:  String,
-
-//     cloudinary_id: String,
-
-//     name: String,
-
-//     price: Number,
-
-//     quantity:  Number,
-
-//   }],
-
-//     delivery: {
-//       address: { type: String },
-//       city: { type: String },
-//       postalCode: { type: String },
-//       country: { type: String },
-//     },
-//     totalPrice: Number,
-//     orderNumber: String,
-//     paymentMethod: { type: String },
-//     shippingPrice: { type: Number },
-//     isPaid: { type: Boolean, default: false },
-//     paidAt: { type: Date },
-//     isDelivered: { type: Boolean, default: false },
-//     deliveredAt: { type: Date },
-//   },
-//   { timestamps: true }
-// );
-
-// module.exports = mongoose.model('Order', OrderSchema);
