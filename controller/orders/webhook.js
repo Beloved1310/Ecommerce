529012fc752d83eaf04ba01b13366fcc7b338e63
Beloved1310 <adeoluwafisayomi@gmail.com>
2021-06-07 17:55:47 +0100

    refactor!: change webhook route

--- controller/orders/webhook.js
+++ controller/orders/webhook.js
@@ -1,3 +1,5 @@
+/* eslint camelcase: "off" */
+
 const Order = require('../../Model/Order');
 const webhookValidation = require('../../validation/webhookValidation');
 const Payment = require('../../Model/Payment');
@@ -6,16 +8,26 @@ const { MY_HASH } = require('../../config');
 module.exports = async (req, res) => {
   const { value, error } = webhookValidation(req.body);
   if (error) return res.status(400).send({ error: error.details[0].message });
-
+  const { data } = value;
   const hash = req.headers['verify-hash'];
   if (hash !== MY_HASH) return res.send(401, 'Unauthorized User');
+  if (data.status !== 'successful')
+    return res.status(400).send({ message: 'Transaction Unsuccessful' });
 
-  await Order.updateOne({}, { isPaid: true });
-  const { data, status, message } = value;
-  const createdWebhook = await Payment.create({
-    status,
-    message,
+  await Order.updateOne({ orderNumber: value.data.tx_ref }, { isPaid: true });
+
+  await Payment.create({
     data,
   });
-  return res.send({ message: 'Transaction Successful', data: createdWebhook });
+
+  const { id, tx_ref, amount, status, customer } = data;
+  const paymentResponse = {
+    id,
+    tx_ref,
+    amount,
+    status,
+    customer,
+  };
+
+  return res.send({ message: 'Transaction Successful', data: paymentResponse });
 };
