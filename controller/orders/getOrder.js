5a36eed11f60a318659cf2bff0a800b757c79366
Beloved1310 <adeoluwafisayomi@gmail.com>
2021-06-10 16:56:30 +0100

    refactor!: remove if condition for order route

--- controller/orders/getOrder.js
+++ controller/orders/getOrder.js
@@ -3,7 +3,5 @@ const Order = require('../../Model/Order');
 module.exports = async (req, res) => {
   const data = await Order.find({ 'user._id': req.user._id });
 
-  if (!data) return res.status(404).send({ error: 'Order Not Found' });
-
   return res.send({ message: 'Your Order', data });
 };
