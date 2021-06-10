66defed1c81558d913e99501db5f044731de5049
Beloved1310 <adeoluwafisayomi@gmail.com>
2021-06-10 16:50:24 +0100

    refactor: remove if statement to handle error

--- controller/orders/adminGetUserOrder.js
+++ controller/orders/adminGetUserOrder.js
@@ -2,7 +2,5 @@ const Order = require('../../Model/Order');
 
 module.exports = async (req, res) => {
   const data = await Order.find({});
-
-  if (!data) return res.status(404).send({ error: 'Order Not Found' });
   return res.send({ message: 'Order', data });
 };
