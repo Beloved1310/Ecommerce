ca63aeb56d8f4ae4ddde6762cf7050eb8ef5b37c
Beloved1310 <adeoluwafisayomi@gmail.com>
2021-06-10 10:42:38 +0100

    feat: add isAdmin route to user order

--- Routes/order.js
+++ Routes/order.js
@@ -3,11 +3,14 @@ const asyncMiddleware = require('../middleware/async');
 
 const router = express.Router();
 const auth = require('../middleware/auth');
+const isAdmin = require('../middleware/isAdmin');
 const checkout = require('../controller/orders/checkout');
 const webhook = require('../controller/orders/webhook');
 const getOrder = require('../controller/orders/getOrder');
+const adminGetUserOrder = require('../controller/orders/adminGetUserOrder');
 
-router.get('/order/:id', auth, asyncMiddleware(getOrder));
+router.get('/order/me', auth, asyncMiddleware(getOrder));
+router.get('/order/admin', auth, isAdmin, asyncMiddleware(adminGetUserOrder));
 
 router.post('/checkout', auth, asyncMiddleware(checkout));
 
